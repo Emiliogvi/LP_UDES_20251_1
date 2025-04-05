dfac1f4bc22467d4f8d5cff5144f9b624b744cf1
Emilio Gomez <emilio.gomez@gmail.com>
2025-04-05 12:57:47 -0500

    El mejor cambio hasta ahora

--- gamma.py
+++ gamma.py
@@ -3,3 +3,5 @@ print("este archivo contiene el archivo gamma.py")
 #JuanSebastianNavasRamos
 print("emilio un verdadero tonto")
 print("Emilio es muy inteligente")
+print("Emilio es muy fuerte")
+print("Emilio es mejor que villamil")
