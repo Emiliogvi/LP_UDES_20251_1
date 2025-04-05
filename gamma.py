e7883acd71cb33d405b583b801ffb4c31e37bfdb
Laboratorio Sala Electrónica <salaelectronica@udes.edu.co>
2025-04-05 12:51:38 -0500

    Se agrego nombre b

--- gamma.py
+++ gamma.py
@@ -1,8 +1,3 @@
 print("este archivo contiene el archivo gamma.py")
-<<<<<<< HEAD
-print("emilio un verdadero tonto")
-=======
-
+#JohanSebastianVillamil
 #JuanSebastianNavasRamos
- 
->>>>>>> f136a1e2909c49226067097476d2c2a7dfe62d59
